89943652c098b94005f8be5f00c6c77762d41b84
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2023-10-26 17:48:10 +0530

    Create newfile.py

---
+++ newfile.py
@@ -0,0 +1,49 @@
+import requests
+import time
+import telebot
+
+print("Server is Running......")
+bot = telebot.TeleBot("6339536087:AAF_SMbdKV0Pku9lioxG2FWagPCYW83jGFU")
+DHONIPRIME_URL = 'https://dhoniprime.firebaseio.com/Match.json'
+
+
+def get_urls() -> list[str]:
+    r = requests.get(url=DHONIPRIME_URL).json()
+    links = ""
+    for k, v in r.items():
+        # links = v['posttitle' + "\nBe Active ;)"] + ' -> '
+        links = "https://t.me/DhOniFantasyPrimeLeaKs "
+        links += v['matchLink'] if v.get(
+            'matchLink') is not None else v['posttitle'] + "\nBe Active ;)"
+        links += " @DhOniFantasyPrimeLeaKs"
+        links += "\n\n➡️ DhOni FinaL \n\n➡️ Join Channel : @DhOniFantasyPrimeLeaKs\n➡️ T.me/DhOniFantasyPrimeLeaKs"
+    return links
+
+
+def get_url() -> list[str]:
+    r = requests.get(url=DHONIPRIME_URL).json()
+    links = ""
+    for k, v in r.items():
+        links = v['matchLink'] if v.get(
+            'matchLink') is not None else "Team Coming"
+        links += "\n\nFinaL"
+    return links
+
+
+global text
+text = get_urls()
+texts = get_url()
+while (True):
+    text_new = get_urls()
+    text_news = get_url()
+    if text_new != text:
+        bot.send_message(chat_id='-1001481256781',
+                         text=text_new, disable_web_page_preview=True)
+        bot.send_message(chat_id='-1001950168101',
+                         text=text_news, disable_web_page_preview=True)
+        print("DhOni Links uploaded! Sent to channel")
+        text = text_new
+    time.sleep(0.1)
+
+
+bot.infinity_polling()
